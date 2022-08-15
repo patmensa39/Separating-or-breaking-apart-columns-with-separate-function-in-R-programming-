c79d17b43ebabed63cdc808e263b5fd92ab6aa3d
Patrick Mensah <70266683+patmensa39@users.noreply.github.com>
2022-08-15 10:22:41 -0400

    Add files via upload

---
+++ Separating or breaking apart columns with  separate().R
@@ -0,0 +1,31 @@
+# Breaking apart colums in R programming 
+library(tidyverse)
+
+### US government Center for Medicare and Medicaid services
+
+### Reading the data 
+
+### Using your own names for the columns 
+names <- c('DRG', 'ProviderID', 'Name','Address', 'City', 'State', 'ZipCode',
+           'Region', 'Discharges', 'AverageCharges', 'AverageTotalPayments', 
+           'AverageMedicarePayments')
+
+inpatient <- read_tsv('http://594442.youcanlearnit.net/inpatient.tsv', 
+                      col_names = names, skip = 1)
+glimpse(inpatient)
+
+### Looking at the unique values of the DRG 
+unique(inpatient$DRG)
+
+### Separating DRG by -
+separate(data = inpatient, col = DRG, into = c("DRGCode", "DRGDescription"), sep = '-')
+
+### You realise some of the values did not go through so let take a look at this values
+inpatient$DRG[45895]
+### You realise drug eluting has dash in them and this is the reason 
+
+### Another way is to break or separate these columns by the forth item
+inpatient <- separate(data = inpatient, col = DRG, into = c("DRGCode", "DRGDescription"), sep = 4)
+glimpse(inpatient)
+
+
